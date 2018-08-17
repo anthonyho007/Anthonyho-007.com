5b345ede8425758995324085b32dd720fd4b2526
Anthony Ho <anthonyho007@gmail.com>
2018-08-16 23:15:43 -0700

    Added experience page

--- middleware/app.go
+++ middleware/app.go
@@ -10,6 +10,7 @@ func App(port string) {
 	mux.HandlerFunc("GET", "/", HandleMainPage)
 	mux.HandlerFunc("GET", "/about", HandleAboutPage)
 	mux.HandlerFunc("GET", "/projects", HandleProjectsPage)
+	mux.HandlerFunc("GET", "/experiences", HandleExperiencePage)
 	mux.ServeFiles(
 		"/assets/*filepath",
 		http.Dir("assets/"),
--- middleware/handle_main.go
+++ middleware/handle_main.go
@@ -15,3 +15,7 @@ func HandleAboutPage(writer http.ResponseWriter, req *http.Request) {
 func HandleProjectsPage(writer http.ResponseWriter, req *http.Request) {
 	RenderTemplate(writer, req, "index/projects", nil)
 }
+
+func HandleExperiencePage(writer http.ResponseWriter, req *http.Request) {
+	RenderTemplate(writer, req, "index/experiences", nil)
+}
